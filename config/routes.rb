d9bd73d25a59c81955297a0b0941cc11fc0669f2
Yuji.Yamaguchi <ieirireon@gmail.com>
2019-09-21 10:56:07 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
+  # Sessionルート
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # Userルート
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 end
